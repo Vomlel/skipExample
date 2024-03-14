50e54544464885b3b4522a8c9ff6f7ae14c98e46
Vomlel <jv@2scale.cz>
2024-03-14 11:19:41 +0100

    -

--- cypress/e2e/C111111.cy.js
+++ cypress/e2e/C111111.cy.js
@@ -1,5 +1,3 @@
-let skipTests = false
-
 describe('Example skipping', () => {
 
   it('failig test', () => {
@@ -7,25 +5,26 @@ describe('Example skipping', () => {
     cy.on('fail', (err, runnable) => {
       // you can check here what kind of exception it is and adapt behavior (like - if a backend is not running - then skip tests that need to use the backend)
       Cypress.env('skipTests', true);
-      throw err
+      // throw err
     })
 
     cy.visit("http://vommy.cz")
     cy.get("#toggleButton").click()
-    cy.wait(2000)
+    cy.wait(500)
     cy.get("#elementContainer").should("be.visible")
     cy.get("#toggleButton").click()
-    cy.wait(2000)
+    cy.wait(500)
     cy.get("#elementContainer").should("be.visible")
+    cy.onlyOn(!Cypress.env('skipTests'))
   })
 
   it('passing test', () => {
     cy.visit("http://vommy.cz")
     cy.get("#toggleButton").click()
-    cy.wait(2000)
+    cy.wait(500)
     cy.get("#elementContainer").should("be.visible")
     cy.get("#toggleButton").click()
-    cy.wait(2000)
+    cy.wait(500)
     cy.get("#elementContainer").should("not.be.visible")
   })
 
@@ -33,10 +32,10 @@ describe('Example skipping', () => {
     cy.onlyOn(!Cypress.env('skipTests'))
     cy.visit("http://vommy.cz")
     cy.get("#toggleButton").click()
-    cy.wait(2000)
+    cy.wait(500)
     cy.get("#elementContainer").should("be.visible")
     cy.get("#toggleButton").click()
-    cy.wait(2000)
+    cy.wait(500)
     cy.get("#elementContainer").should("not.be.visible")
   })
 })
