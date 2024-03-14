1388bd2ffe598db8bb79b019983e47fdc8e1db9f
Vomlel <jv@2scale.cz>
2024-03-14 11:58:14 +0100

    finish example

--- cypress/e2e/C111111.cy.js
+++ cypress/e2e/C111111.cy.js
@@ -1,13 +1,17 @@
-import {onlyOn, skipOn} from '@cypress/skip-test'
+import { skipOn } from '@cypress/skip-test'
 
 describe('Example skipping', () => {
 
   it('failing -> skipping test', () => {
 
     cy.on('fail', (err, runnable) => {
-      // you can check here what kind of exception it is and adapt behavior (like - if a backend is not running - then skip tests that need to use the backend)
-      Cypress.env('skipTests', true);
-      skipOn(Cypress.env('skipTests'))
+      if
+        // you can check here what kind of exception it is and adapt behavior (like - if a backend is not running - then skip tests that need to use the backend)
+      (true) {
+        Cypress.env('skipTests', true);
+        skipOn(Cypress.env('skipTests'))
+      }
+
       throw err // this will be not thrown but skipped
     })
 
@@ -17,7 +21,6 @@ describe('Example skipping', () => {
     cy.get("#elementContainer").should("be.visible")
     cy.get("#toggleButton").click()
     cy.wait(500)
-    // cy.skipOn(!Cypress.env('skipTests'))
     cy.get("#elementContainer", {timeout: 500}).should("be.visible")
   })
 
