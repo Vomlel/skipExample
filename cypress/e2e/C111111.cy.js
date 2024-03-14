9fedcfcfce793099e7b9f575d14f375b3fd71d5c
Vomlel <jv@2scale.cz>
2024-03-14 10:49:08 +0100

    skipping

---
+++ cypress/e2e/C111111.cy.js
@@ -0,0 +1,42 @@
+let skipTests = false
+
+describe('Example skipping', () => {
+
+  it('failig test', () => {
+
+    cy.on('fail', (err, runnable) => {
+      // you can check here what kind of exception it is and adapt behavior (like - if a backend is not running - then skip tests that need to use the backend)
+      Cypress.env('skipTests', true);
+      throw err
+    })
+
+    cy.visit("http://vommy.cz")
+    cy.get("#toggleButton").click()
+    cy.wait(2000)
+    cy.get("#elementContainer").should("be.visible")
+    cy.get("#toggleButton").click()
+    cy.wait(2000)
+    cy.get("#elementContainer").should("be.visible")
+  })
+
+  it('passing test', () => {
+    cy.visit("http://vommy.cz")
+    cy.get("#toggleButton").click()
+    cy.wait(2000)
+    cy.get("#elementContainer").should("be.visible")
+    cy.get("#toggleButton").click()
+    cy.wait(2000)
+    cy.get("#elementContainer").should("not.be.visible")
+  })
+
+  it('skipped test', () => {
+    cy.onlyOn(!Cypress.env('skipTests'))
+    cy.visit("http://vommy.cz")
+    cy.get("#toggleButton").click()
+    cy.wait(2000)
+    cy.get("#elementContainer").should("be.visible")
+    cy.get("#toggleButton").click()
+    cy.wait(2000)
+    cy.get("#elementContainer").should("not.be.visible")
+  })
+})
